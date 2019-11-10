222bc5edac652307d0d1143519bc0553d809f368
maopch <maopch@yonyou.com>
2019-11-10 20:24:51 +0800

    login页面

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import Hello from './main/index';
+import Login from './main/index';
 
 class App extends Component {
     render() {
-        return <Hello />;
+        return <Login />;
     }
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { News } from './News';
 import { Shows } from './Shows';
 import { Tools } from './Tools';
+import { hcc } from './hcc';
 
-export { News, Shows, Tools };
+export { News, Shows, Tools, hcc };
