93321e95e288f2793adc53661187271498d3425f
maopch <maopch@yonyou.com>
2019-11-11 23:15:37 +0800

    login ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import './App.css';
-import Hello from './main/layout';
+import Login from './hcc/Login';
 
 class App extends Component {
     render() {
-        return <Hello />;
+        return <Login />;
+        //     return (
+        //         <Router>
+        //     <Link to='/'>root</Link> <br />
+        //     <Link to='/hello'>hello</Link> <br />
+        //     <Link to='/todolist'>todolist</Link>
+        //     <div>
+        //       <Route path='/'  exact render={() => {
+        //         return <div>root page</div>
+        //       }} />
+        //       <Route path='/hello' render={() => {
+        //         return <div>hello world</div>
+        //       }} />
+        //       <Route path='/todolist' component={TodoList} />
+        //     </div>
+        //   </Router>
+        //     )
     }
 }
 
